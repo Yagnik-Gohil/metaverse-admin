4461eea956c17d5b70a4fb2d00145670b1fcad17
Yagnik-Gohil <gohilyagnik3@gmail.com>
2024-11-26 21:56:53 +0530

    fix: sidebar design

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,7 @@
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
 import { logout } from "@/api/auth.service";
+import { FaCircleUser, FaCrown, FaMap } from "react-icons/fa6";
 
 const SideBar = () => {
   const navigate = useNavigate();
@@ -13,28 +14,51 @@ const SideBar = () => {
   return (
     <div className="bg-gray-800 text-white w-64 h-full flex flex-col select-none">
       <div className="p-4 border-b border-gray-700">
-        <Link to={"/"}>
+        <NavLink to={"/"} 
+            style={{ textDecoration: "none", color: "white" }}>
           <h1 className="text-xl font-bold">Metaverse</h1>
-        </Link>
+        </NavLink>
       </div>
 
       <nav className="flex-grow p-4">
         <ul className="space-y-4">
-          <li className="hover:bg-gray-700 p-2 rounded">
-            <Link to={"/user"} className="block">
-              Users
-            </Link>
-          </li>
-          <li className="hover:bg-gray-700 p-2 rounded">
-            <Link to={"/map"} className="block">
-              Map
-            </Link>
-          </li>
-          <li className="hover:bg-gray-700 p-2 rounded">
-            <Link to={"/admin"} className="block">
-              Admin
-            </Link>
-          </li>
+          <NavLink
+            style={{ textDecoration: "none", color: "white" }}
+            to={"/user"}
+            className={({ isActive }) =>
+              `p-2 rounded flex items-center gap-4 ${
+                isActive ? "bg-blue-600" : "hover:bg-gray-700"
+              }`
+            }
+          >
+            <FaCircleUser size={18} />
+            <span>Users</span>
+          </NavLink>
+          <NavLink
+            style={{ textDecoration: "none", color: "white" }}
+            to={"/map"}
+            className={({ isActive }) =>
+              `p-2 rounded flex items-center gap-4 ${
+                isActive ? "bg-blue-600" : "hover:bg-gray-700"
+              }`
+            }
+          >
+            <FaMap size={18} />
+            <span>Map</span>
+          </NavLink>
+          <NavLink
+            
+            style={{ textDecoration: "none", color: "white" }}
+            to={"/admin"}
+            className={({ isActive }) =>
+              `p-2 rounded flex items-center gap-4 ${
+                isActive ? "bg-blue-600" : "hover:bg-gray-700"
+              }`
+            }
+          >
+            <FaCrown size={18} />
+            <span>Admin</span>
+          </NavLink>
         </ul>
       </nav>
 
